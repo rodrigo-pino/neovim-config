af7d71397b2acabc2aba484a490431138f05553c
Rodrigo <rodrodpino@gmail.com>
2025-09-13 00:30:30 +0100

    feat: improve key maps in for lsp and telescope

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,18 +4,39 @@ return {
 		config = function()
 			-- slight improvement to diagnostics
 			local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = signs.Error,
+						[vim.diagnostic.severity.WARN] = signs.Warn,
+						[vim.diagnostic.severity.HINT] = signs.Hint,
+						[vim.diagnostic.severity.INFO] = signs.Info,
+					},
+					numhl = {
+						[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+						[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+						[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+						[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					},
+				},
 
-			vim.diagnostic.config({ virtual_text = true })
+				virtual_text = true,
+			})
+
 			-- lsp key maps --
 			-- note: diagnostics are not exclusive to lsp servers
 			-- so these can be global key bindings
 			vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+			vim.keymap.set("n", "gK", function()
+				local new_config = {
+					current_line = not vim.diagnostic.config().virtual_lines,
+				}
+
+				vim.diagnostic.config({ virtual_lines = new_config })
+			end, { desc = "Toggle diagnostic virtual_lines" })
 
 			vim.api.nvim_create_autocmd("lspattach", {
 				desc = "lsp actions",
@@ -25,54 +46,47 @@ return {
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
 					vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>t", vim.lsp.buf.type_definition, opts)
+
 					-- this i usally access through telescope
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+					-- not sure what this is supposed to do
+					vim.keymap.set("n", "gdc", vim.lsp.buf.declaration, opts)
+					-- not sure how this one is supposed to work
 					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					-- I use telescope to find references
+					-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 					vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set({ "n", "x" }, "<leader>ff", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
+					-- Formatting with conform.nvim and not the LSP
+					-- vim.keymap.set({ "n", "x" }, "<leader>ff", function()
+					-- 	vim.lsp.buf.format({ async = true })
+					-- end, opts)
 				end,
 			})
+
+			-- list of servers enabled
+			-- Go
+			vim.lsp.enable("gopls")
+			vim.lsp.enable("golangci_lint_ls")
+			-- Rust
+			vim.lsp.enable("rust_analyzer")
+			-- Lua
+			vim.lsp.enable("lua_ls")
+			-- TOML
+			vim.lsp.enable("taplo")
+			-- Bash
+			vim.lsp.enable("bashls")
+			-- LaTex
+			vim.lsp.enable("texlab")
+			-- Markdown
+			vim.lsp.enable("marksman")
 		end,
 	},
 
-	-- lsp helper, mason
+	-- lsp package manager: mason
 	{
 		"mason-org/mason.nvim",
+		opts = {},
 		run = ":masonupdate",
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp", -- autocomplete based on buffer diagnostics
-		},
-
-		config = function()
-			-- important setup for lspconfig (based on lsp zero)
-			-- defining it here since it needs: cmp, lsp_config and mason
-			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local default_setup = function(server)
-				require("lspconfig")[server].setup({
-					capabilites = lsp_capabilities,
-				})
-			end
-
-			-- custom lsp configuration should come here
-
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = {},
-				handlers = {
-					default_setup,
-				},
-			})
-		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,33 +6,33 @@ return {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
-        config = function()
-            local telescope = require("telescope")
-            -- load extensions
-            telescope.load_extension("fzf")
-            -- set key maps
-            local builtin = require("telescope.builtin")
-            -- most used base ones
-            vim.keymap.set("n", "<space>f", builtin.find_files, {})
-            vim.keymap.set("n", "<space>l", builtin.live_grep, {})
-            vim.keymap.set("n", "<space>b", builtin.buffers, {})
-            -- least used ones
-            vim.keymap.set("n", "<leader>fg", builtin.grep_string, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-            vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, {})
-            -- for git
-            vim.keymap.set("n", "<leader>fgc", builtin.git_commits, {})
-            vim.keymap.set("n", "<leader>fgs", builtin.git_status, {})
-            vim.keymap.set("n", "<leader>fgt", builtin.git_stash, {})
-            -- for lsp
-            vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
-            vim.keymap.set("n", "<leader>fdd", builtin.diagnostics, {})
-            vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, {})
-            vim.keymap.set("n", "<leader>ftd", builtin.lsp_type_definitions, {})
-            -- for treesitter
-            vim.keymap.set("n", "<leader>ft", builtin.treesitter, {})
-            -- no idea
-            vim.keymap.set("n", "<leader>fx", builtin.builtin, {})
-        end
+		config = function()
+			local telescope = require("telescope")
+			-- load extensions
+			telescope.load_extension("fzf")
+			-- set key maps
+			local builtin = require("telescope.builtin")
+			-- most used base ones
+			vim.keymap.set("n", "<space>f", builtin.find_files, {})
+			vim.keymap.set("n", "<space>l", builtin.live_grep, {})
+			vim.keymap.set("n", "<space>b", builtin.buffers, {})
+			vim.keymap.set("n", "<space>sd", builtin.diagnostics, {})
+			-- least used ones
+			vim.keymap.set("n", "<leader>ff", builtin.current_buffer_fuzzy_find, {})
+			vim.keymap.set("n", "<leader>fg", builtin.grep_string, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			-- for git
+			vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+			vim.keymap.set("n", "<leader>gt", builtin.git_status, {})
+			vim.keymap.set("n", "<leader>gs", builtin.git_stash, {})
+			-- for lsp
+			vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+			vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, {})
+			vim.keymap.set("n", "<leader>ftd", builtin.lsp_type_definitions, {})
+			-- for treesitter
+			vim.keymap.set("n", "<leader>ft", builtin.treesitter, {})
+			-- no idea
+			vim.keymap.set("n", "<leader>fx", builtin.builtin, {})
+		end,
 	},
 }
