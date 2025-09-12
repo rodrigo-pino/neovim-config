679c6150e6d0bcab06dec41de3479220c8f3a25d
Rodrigo <rodrodpino@gmail.com>
2025-09-12 20:29:23 +0100

    chore: minor improvements

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,29 +1,29 @@
 return {
-    {
+	{
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
 		lazy = false,
-        config = function()
-            vim.opt.termguicolors = true
-            require("nvim-tree").setup({
-                view = {
-                    centralize_selection = true,
-                    -- width = 50,
-                    float = {
-                        enable = false,
-                    },
-                },
-                diagnostics = {
-                    enable = true,
-                },
-            })
+		config = function()
+			vim.opt.termguicolors = true
+			require("nvim-tree").setup({
+				view = {
+					centralize_selection = true,
+					-- width = 50,
+					float = {
+						enable = false,
+					},
+				},
+				diagnostics = {
+					enable = true,
+				},
+			})
 
-            -- mappings
-            local api = require("nvim-tree.api")
-            vim.keymap.set("n", "<space>e", api.tree.toggle, { silent = true })
-        end
-    },
+			-- mappings
+			local api = require("nvim-tree.api")
+			vim.keymap.set("n", "<space>e", ":NvimTreeFindFileToggle<CR>", { silent = true })
+		end,
+	},
 }
